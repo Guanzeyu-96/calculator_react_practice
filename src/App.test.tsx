0d8300aa141ebc7e9effba0fd8fdfcc64f85be1f
Zeyu Guan <zeyu.guan@thoughtworks.com>
2022-11-24 10:48:05 +0800

    [guanzy]test: add new test: calculate number by click

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -11,9 +11,9 @@ describe("Keyboard input test", () => {
     expect(element).not.toHaveTextContent("1");
 
     // type '1' with keyboard
-    userEvent.keyboard('1');
+    userEvent.keyboard("1");
 
-    expect(element).toHaveTextContent('1');
+    expect(element).toHaveTextContent("1");
   });
 
   it("should calculate numbers by keyboard", function () {
@@ -22,24 +22,70 @@ describe("Keyboard input test", () => {
 
     // type '1' '+' '1' 'Enter' with keyboard
     userEvent.keyboard("1+1{Enter}");
-    expect(element).toHaveTextContent('2');
+    expect(element).toHaveTextContent("2");
 
     // reset
     userEvent.keyboard("{Esc}");
-    expect(element).toHaveTextContent('0');
+    expect(element).toHaveTextContent("0");
 
     // type '3' '*' '4' 'Enter' with keyboard
     userEvent.keyboard("3*4{Enter}");
-    expect(element).toHaveTextContent('12');
+    expect(element).toHaveTextContent("12");
 
     // type '9' '-' '1' 'Enter' with keyboard
     userEvent.keyboard("{Esc}");
     userEvent.keyboard("9-1{Enter}");
-    expect(element).toHaveTextContent('8');
+    expect(element).toHaveTextContent("8");
 
     // type '12' '/' '3' 'Enter' with keyboard
     userEvent.keyboard("{Esc}");
     userEvent.keyboard("12/3{Enter}");
-    expect(element).toHaveTextContent('4');
+    expect(element).toHaveTextContent("4");
+  });
+
+  it("should calculate numbers by click", function () {
+    render(<App />);
+    const element = screen.getByTestId("screen");
+    const key1 = screen.getByText("1");
+    const key2 = screen.getByText("2");
+    const keyPlus = screen.getByText("+");
+    const keyMinus = screen.getByText("-");
+    const keyTimes = screen.getByText("x");
+    const keyDivide = screen.getByText("÷");
+    const keyEqual = screen.getByText("=");
+    const keyReset = screen.getByText("C");
+
+    // // type '1' '+' '2' '=' with click
+    userEvent.click(key1);
+    userEvent.click(keyPlus);
+    userEvent.click(key2);
+    userEvent.click(keyEqual);
+    expect(element).toHaveTextContent("3");
+
+    // reset
+    userEvent.click(keyReset);
+    expect(element).toHaveTextContent("0");
+
+    // type '2' '*' '2' '=' with click
+    userEvent.click(key2);
+    userEvent.click(keyTimes);
+    userEvent.click(key2);
+    userEvent.click(keyEqual);
+    expect(element).toHaveTextContent("4");
+
+    // type '2' '-' '1' '=' with click
+    userEvent.click(keyReset);
+    userEvent.click(key2);
+    userEvent.click(keyMinus);
+    userEvent.click(key1);
+    userEvent.click(keyEqual);
+
+    // type '1' '/' '2' '=' with click
+    userEvent.click(keyReset);
+    userEvent.click(key1);
+    userEvent.click(keyDivide);
+    userEvent.click(key2);
+    userEvent.click(keyEqual);
+    expect(element).toHaveTextContent("0.5");
   });
 });
