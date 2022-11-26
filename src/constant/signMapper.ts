231937b33594376c145f60f827b9fad35e0d6409
Zeyu Guan <zeyu.guan@thoughtworks.com>
2022-11-26 17:03:02 +0800

    [guanzy]feat: use scientificComputing as an util function in redux reducer

---
+++ src/constant/signMapper.ts
@@ -0,0 +1,6 @@
+export const signMapper: any = {
+  "+": "+",
+  "-": "-",
+  x: "*",
+  "÷": "/",
+};
--- src/store/calculator.ts
+++ src/store/calculator.ts
@@ -1,10 +1,12 @@
 import { createSlice } from "@reduxjs/toolkit";
 import { Decimal } from "decimal.js";
+import { scientificComputing } from "../utils/scientific-computing";
+import { eraseZero } from "../utils/erase-zero";
+import { signMapper } from "../constant/signMapper";
 
 interface CalculatorState {
   input: string; // 实时输入区
   stage: string | null; // 暂存区
-  rule: string | null; // 计算法则存储区
   result: string | null; // 计算结果保存区
   isResult: boolean; // 是否为input阶段,仅用来控制显示（input阶段显示input，非input阶段显示result）
   isNumInit: boolean; // 判断num输入状态。为true时表现为重置；为false时表现为拼接
@@ -13,7 +15,6 @@ interface CalculatorState {
 const initCalculatorState: CalculatorState = {
   input: "0",
   stage: null,
-  rule: null,
   result: null,
   isResult: false,
   isNumInit: false,
@@ -62,20 +63,29 @@ export const calculatorSlice = createSlice({
 
     operatorClickHandler(state, action) {
       // 如果点击运算符的时候result区有数据，则执行连算模式
+      // 连续运算指的是：暂存区清空，然后result区置入input区，result区清空
       if (state.isResult) {
+        state.stage = null;
         state.input = state.result!;
         state.isResult = false;
         state.result = null;
       }
 
-      // 点击运算符发生的事情：把input区的结果放入stage、初始化input区、把运算符的value记录到rule区，数字输入状态变为初始化
-      state.stage = state.input;
-      state.rule = action.payload as string;
+      // 点击运算符发生的事情：把input区的结果拼接入stage、然后把运算符拼接到stage区之后，数字输入状态变为初始化
+      if (!state.stage) {
+        state.stage =
+          eraseZero(state.input) + signMapper[action.payload as string];
+      } else {
+        state.stage =
+          state.stage +
+          eraseZero(state.input) +
+          signMapper[action.payload as string];
+      }
+
       state.isNumInit = true;
     },
     equalClickHandler(state) {
-      // 点击=发生的事：根据input区，stage区和rule区的数据计算出结果并放入result区，数字输入状态变为初始化
-      // 数值计算前使用Decimal处理
+      // 点击=发生的事：把input区的数据拼接到stage区之后得到expression，计算出结果并放入result区，数字输入状态变为初始化
       // 判断用户没有输入运算符点击=的情况
       if (!state.stage) {
         state.result = state.input;
@@ -84,27 +94,8 @@ export const calculatorSlice = createSlice({
         return;
       }
 
-      const value = new Decimal(state.stage!);
-      const targetValue = new Decimal(state.input);
-
-      switch (state.rule) {
-        case "+":
-          state.result = value.add(targetValue).toString();
-          break;
-        case "-":
-          state.result = value.minus(targetValue).toString();
-          break;
-        case "x":
-          state.result = value.times(targetValue).toString();
-          break;
-        case "÷":
-          // 被除数为0的情况下抛出异常
-          if (+state.input === 0) throw new Error("Dividend can't be 0!");
-          state.result = value.div(targetValue).toString();
-          break;
-        default:
-          state.result = state.input; // default对应用户没有输入运算符点击=的情况
-      }
+      const expression = state.stage + eraseZero(state.input);
+      state.result = scientificComputing(expression);
 
       state.isResult = true;
       state.isNumInit = true;
@@ -113,7 +104,6 @@ export const calculatorSlice = createSlice({
       // 重置按钮，按c使得所有state更新为最初状态
       state.input = "0";
       state.stage = null;
-      state.rule = null;
       state.result = null;
       state.isResult = false;
       state.isNumInit = false;
